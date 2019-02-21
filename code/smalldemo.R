2f2746faa6afb3926a37b301b2e896c708c89cec
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-21 15:35:20 -0600

    Filled out structure of smalldemo.R script.

--- code/smalldemo.R
+++ code/smalldemo.R
@@ -2,5 +2,35 @@
 library(MASS)
 source("toydata.R")
 
+# SCRIPT PARAMETERS
+# -----------------
+scenarios <- c("zh1","zh2","zh3","zh4")
+methods   <- c("lasso")
+
 # Initialize the sequence of pseudorandom numbers.
 set.seed(1)
+
+# Repeat for each simulation scenario.
+for (scenario in scenarios) {
+
+  # GENERATE DATA
+  # -------------
+  cat(sprintf("Generating data for \"%s\" scenario.\n",scenario))
+  # TO DO.
+
+  # Repeat for each method.
+  for (method in methods) {
+
+    # FIT MODEL
+    # ---------
+    # TO DO.
+
+    # PREDICT TEST OUTCOMES
+    # ---------------------
+    # TO DO.
+
+    # EVALUATE PREDICTIONS
+    # --------------------
+    # TO DO.
+  }
+}

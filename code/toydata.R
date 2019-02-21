048c10a8ee25f1cd303aa5977b57a80155f8a0ef
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-21 15:26:25 -0600

    Implemented function simulate_predictors.

--- code/toydata.R
+++ code/toydata.R
@@ -44,53 +44,18 @@ simulate_predictors_grouped <- function (n, p, s = 0.1) {
   return(X)
 }
 
-# TO DO: Explain here what this function does.
-simulate_predictors <- function (n, p, design = c("corr", "decaying_corr",
-                                                  "grouped")) {
-
-}
-
-# Simulate design matrix X under different scenarios, specifically the
-# scenarios from Zhou and Hastie (2005).
-X_sim = function(n, p, design = c("decreasing_corr", "equal_corr",
-                                  "grouped", "independent")) {
-  design = match.arg(design)
-
-  if (design == "decreasing_corr") {
-    Sigma = 0.5 ^ abs(outer(1:p, 1:p, FUN = "-"))
-    X = MASS::mvrnorm(n = n, rep(0, p), Sigma)
-  }
-
-  if (design == "equal_corr") {
-    Sigma = matrix(data = 0.5,
-                   nrow = p,
-                   ncol = p)
-    diag(Sigma) = 1
-    X = MASS::mvrnorm(n = n, rep(0, p), Sigma)
-  }
-  if (design == "grouped") {
-    X = matrix(data = 0, nrow = n, ncol = p)
-    Z = matrix(rnorm(n*3, mean = 0, sd = 1),nrow=n,ncol=3)
-
-    # This defines the groups in the paper.
-    q = c(round(p/8),round(p/4),round(3*p/8),p) 
-    E = matrix(rnorm(n * p, mean = 0, sd = 0.1),nrow = n)
-
-    # This last group is N(0,1).
-    E[,(q[3]+1):q[4]] = E[,(q[3]+1):q[4]]*10 
-
-    # group 1: Z1 + Normal(0, 0.01)
-    # group 2: Z2 + Normal(0, 0.01)
-    # group 3: Z3 + Normal(0, 0.01)
-    X[, 1:q[1]] = Z[,1]
-    X[, (q[1]+1):q[2]] = Z[,2]
-    X[, (q[2]+1):q[3]] = Z[,3]
-    X = X + E
-  }
-  if (design == "independent") {
-    X = matrix(rnorm(n*p), nrow = n, ncol = p)
-  }
-
+# Return a matrix of samples drawn according to the chosen scenario
+# (see the "design" input argument). The three different designs are
+# meant to recapitulate the three different procedures used to
+# simulate the predictors in Zou & Hastie (2005).
+simulate_predictors <-
+  function (n, p, design = c("corr", "decaying_corr", "grouped")) {
+  design <- match.arg(design)
+  if (design == "corr")
+    X <- simulate_predictors_corr(n,p,0.5)
+  else if (design == "decaying_corr")
+    X <- simulate_predictors_decaying_corr(n,p,0.5)
+  else if (design == "grouped")
+    X <- simulate_predictors_grouped(n,p,0.1)
   return(X)
 }
- 

5ed7db00e8f6b5b748d4987e538870b7dc159506
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-26 23:02:13 -0600

    Added description of function fit_lasso.

--- code/fit.R
+++ code/fit.R
@@ -1,10 +1,11 @@
 # TO DO: Summarize the contents of this file.
 
-# NOTES:
-#   - X should be an n x p numeric matrix.
-#   - y should be a numeric vector of length n.
-#   - nfolds is a parameter to cv.glmnet.
-#   - Returns: (1) the fitted glmnet object, (2) the output of cv.glmnet.
+# Fit a Lasso model to the data, and estimate the penalty strength
+# (lambda) using cross-validation. Input X should be an n x p numeric
+# matrix, and input y should be a numeric vector of length n. Input
+# "nfolds" the number of folds used in the cross-validation. The
+# return value is a list with two elements: (1) the fitted glmnet
+# object, (2) the output of cv.glmnet.
 fit_lasso <- function (X, y, nfolds = 10) {
   out.cv <- glmnet::cv.glmnet(X,y,nfolds = nfolds)
   fit    <- glmnet::glmnet(X,y,standardize = FALSE)

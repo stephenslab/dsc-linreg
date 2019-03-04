535e7d111e7f04f84f7baa955912be11c7515dcf
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-03-04 14:32:04 -0600

    Removed error checking in predict functions.

--- code/predict.R
+++ code/predict.R
@@ -16,17 +16,9 @@ predict_lasso <- function (lasso, X)
 # Use the fitted Elastic Net model, "en", to predict n continuous
 # outcomes given n x p matrix of observations X. The return value is a
 # vector of length n.
-predict_elastic_net <- function (en, X) {
-  if (!inherits(en$fit,"glmnet"))
-    stop("Input argument \"en$fit\" should be a \"glmnet\" object")
-  fit    <- en$fit
-  lambda <- en$cv$lambda.min
-  return(drop(predict(fit,X,lambda)))
-}
+predict_elastic_net <- function (en, X)
+  return(drop(predict(en$fit,X,en$cv$lambda.min)))
 
 # TO DO: Explain here what this function does, and how to use it.
-predict_varbvs <- function (fit, X) {
-  if (!inherits(fit,"varbvs"))
-    stop("Input argument \"fit\" should be a \"varbvs\" object")
+predict_varbvs <- function (fit, X)
   return(predict(fit,X))
-}

e40dc65d038fc5d9b18de1c62bfd157813ba7de8
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-04-09 11:32:00 -0500

    Fixed a few bugs in simulate modules.

--- dsc/modules/simulate/null.R
+++ dsc/modules/simulate/null.R
@@ -1,9 +1,10 @@
 # This R script implements the "null" module in the linreg DSC.
 set.seed(seed)
-se         <- 3
-b          <- rep(0,8)
-dat        <- list()
-dat$Xtrain <- simulate_predictors_decaying_corr(40,8,0.5)
-dat$Xtest  <- simulate_predictors_decaying_corr(200,8,0.5)
-dat$ytrain <- simulate_outcomes(Xtrain,b,se)
-dat$ytest  <- simulate_outcomes(Xtest,b,se)
+se     <- 3
+b      <- rep(0,8)
+Xtrain <- simulate_predictors_decaying_corr(40,8,0.5)
+Xtest  <- simulate_predictors_decaying_corr(200,8,0.5)
+ytrain <- simulate_outcomes(Xtrain,b,se)
+ytest  <- simulate_outcomes(Xtest,b,se)
+dat <- list(train = list(X = Xtrain,y = ytrain),
+            test  = list(X = Xtest,y = ytest))
